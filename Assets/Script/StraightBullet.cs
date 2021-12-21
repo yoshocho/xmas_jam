36e89c08e1dbff6ac4cd09fa7e3954c4dd2381b9
HarashimaYoshikazu <harashima318@gmail.com>
2021-12-21 13:24:16 +0900

    enemyBulletBaseとEnemyControllerを追加

---
+++ Assets/Script/StraightBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightBullet : EnemyBulletBase
+{
+    void Start()
+    {
+        if (_playerPos.gameObject)
+        {
+            Vector2 v = _playerPos.position - this.transform.position;
+            v = v.normalized * _bulletSpeed;
+
+            // 速度ベクトルをセットする
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = v;
+        }
+        else
+        {
+            // プレイヤーが居なかったら、すぐ消してしまう
+            Destroy(this.gameObject);
+        }
+    }
+}
