28b9fcc101526c939d95a01ee9aad01e7ec4341c
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-12-21 15:16:13 +0900

    Generatorにインターバルを作成

--- Assets/okui/EnemyGenerator.cs
+++ Assets/okui/EnemyGenerator.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
+    /// <summary>プレハブを格納</summary>
     [SerializeField]
     public GameObject m_enemy;
+    /// <summary>敵の生成インターバル</summary>
     [SerializeField]
-    public 
+    public float m_interval = 0;
+    /// <summary>経過時間</summary>
+    [SerializeField]
+    public float m_time = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_interval = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(m_enemy,new Vector3(-1,1),Quaternion.identity);
+        //時間計測
+        m_time += Time.deltaTime;
         EnemyGenerate();
     }
     public void EnemyGenerate()
     {
+        //経過時間が生成時間になった時
+        if (m_time > m_interval)
+        {
+            //インスタンス生成
+            Instantiate(m_enemy, new Vector3(-1, 1), Quaternion.identity);
+            //経過時間をリセットして再度計測スタート
+            m_time = 0f;
+        }
+
     }
 }
