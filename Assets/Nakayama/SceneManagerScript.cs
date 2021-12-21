87fd6e7500b8e648c32a3866e5d2774b388dcf9c
BlackSnow(PC) <yumi06050425@gmail.com>
2021-12-21 13:40:01 +0900

    シーン切り替えのスクリプトを追加

---
+++ Assets/Nakayama/SceneManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    [SerializeField] string _name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(_name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
