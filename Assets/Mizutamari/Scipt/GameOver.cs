548053f0c93aa37d5be327804483e08f1afa9154
DESKTOP-A427LQ4\vantan <shutaro0415@outlook.jp>
2021-12-22 12:03:29 +0900

    gameoverの時のスクロールの停止と画面を暗くする機能を作りました。

---
+++ Assets/Mizutamari/Scipt/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UniRx;
+using System;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] Image m_gameoverImage;
+    [SerializeField] PlayerScript m_player;
+
+    Subject<Unit> m_scrollStopSubject = new Subject<Unit>();
+
+    public IObservable<Unit> OnScrollStop => m_scrollStopSubject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_player.OnGameOver
+        //    .Subscribe(_ => ScrollStop())
+        //    .AddTo(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_scrollStopSubject.OnNext(Unit.Default);
+            m_gameoverImage.DOFade(0.8f, 2.3f);
+        }
+    }
+    public void ScrollStop()
+    {   
+        //m_gameoverImage.DOFade(0.5f, 3);   
+    }
+}
